45aed07cfe6e3ddcd4be4afe1e2b77bc2a701dcc
Stealthness <stevewestinlondon@gmail.com>
2024-11-30 15:49:05 +0000

    need to work on json

--- flask/app.py
+++ flask/app.py
@@ -1,4 +1,4 @@
-from flask import Flask
+from flask import Flask, request
 
 app = Flask(__name__)
 
@@ -9,11 +9,11 @@ activities = [
         "activity_name": "Regent's Park 10 k",
         "total_time": "60:17",
         "lap_times": [
-            "10:02"
-            "10:00"
-            "10:02"
-            "10:12"
-            "10:00"
+            "10:02",
+            "10:00",
+            "10:02",
+            "10:12",
+            "10:00",
             "10:01"
         ]
     }
@@ -23,6 +23,18 @@ activities = [
 def get_activity():
     return {"activities": activities}
 
+
+@app.post("/activity")
+def add_activity():
+    request_data = request.get_json()
+    # new_activity = {
+    #     "activity_name": request_data["activity"],
+    #     "activity_id": request_data["activity"],
+    #     "total_time": request_data["activity"],
+    #     "lap_times": request_data["activity"]
+    # }
+    # activities.append(new_activity)
+    return f"You sent: {request_data['activity']['activity_name']}"
 
 @app.route('/')
 def index():
