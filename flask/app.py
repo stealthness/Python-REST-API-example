4a510a3371730a3ccaba84802a69f488c637a252
Stealthness <stevewestinlondon@gmail.com>
2024-11-30 14:55:46 +0000

    added first api get request

--- flask/app.py
+++ flask/app.py
@@ -2,6 +2,25 @@ from flask import Flask
 
 app = Flask(__name__)
 
+activities = [
+    {
+        "activity_name": "Regent's Park 10 k",
+        "activity_id": 10008965,
+        "total_time": "60:17",
+        "lap_times": [
+            "10:02"
+            "10:00"
+            "10:02"
+            "10:12"
+            "10:00"
+            "10:01"
+        ]
+    }
+]
+
+@app.get("/activity")
+def get_activity():
+    return {"activities": activities}
 
 @app.route('/')
 def index():
